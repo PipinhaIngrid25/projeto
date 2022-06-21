d21511209bf5661dac63e46d53b2079036fb867f
Alan Cruz Almeida <alanc2326@gmail.com>
2022-06-21 17:51:32 -0300

    react resolvido

--- site/src/page/login/index.js
+++ site/src/page/login/index.js
@@ -1,49 +1,24 @@
-import axios  from 'axios'
 
-import { useState } from 'react'
 import './index.scss'
 
-import { useNavigate } from 'react-router-dom';
-import {} from './api'
-import { Link } from 'react-router-dom';
-import { login } from '../../api/usuarioApi';
-
-export default function Index() {
-    const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
-    const [erro, setErro] = useState('');
-
-    const navigate = useNavigate();
-
-    async function entrarClick() {
-        try {
-            const r = await login(email, senha)
-            navigate('/');
-        }
-        
-        catch (err) {
-            alert('ERROR')
-        }
-
-
-    }
-
-
+export default function index() {
+    
     
     return(
-        <main className='cod'>
-            <div className="login1">
+        
+<main className="Faixa-Login">
+   <div className="login1">
        <div className="cont">
             <h1> Bem-vindo ao Login</h1>
             <p>Acesse sua conta agora mesmo. </p>
-            <div className="botoes">
-                <Link className="cad" to="../menu" > Cadastrar </Link>
-                <Link className="log" to="../menu2"onClick={entrarClick}> Login </Link>
+            <div classNamw="botoes">
+                <a className="cad" href="../menu/index.html"> Cadastrar </a>
+                <a className="log" href="../menu2/index.html"> Login </a>
             </div>
            
        </div>
 
-       <div className="cont2">
+       <div class="cont2">
                 <h2> Criar uma conta </h2>
             <div className="in-1">
                <label for="nome" > nome:</label>
@@ -52,19 +27,17 @@ export default function Index() {
 
             <div className="in-2">
                <label for="email" > email: </label>
-               <input className="text" type="email"value={ email } onChange={e => setEmail(e.target.value)} />
+               <input className="text" type="email" />
             </div>
 
             <div className="in-3">
                 <label for="senha" > senha: </label>
-                <input class="text" type="password"value= { senha } onChange={e => setSenha(e.target.value)} />
+                <input className="text" type="password" />
              </div>
-
-             {erro}
             </div>
 
    </div>
-        </main>
+</main>
     )
 
 }
